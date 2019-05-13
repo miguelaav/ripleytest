e31ddddc02876083d2db9fa0693bf1f3c86c5209
miguel acevedo <miguel.acevedo.ve@gmail.com>
2019-05-12 22:35:01 -0400

    readme

---
+++ readme.md
@@ -0,0 +1,26 @@
+# How to execute application
+
+## Local Installation
+
+Init redis
+```bash
+redis-server /usr/local/etc/redis.conf
+```
+
+Init Backend
+```bash
+cd ripley
+nodemon main.js
+```
+
+Init Frontend
+```bash
+cd ripleyapp
+npm start
+```
+
+Application Heroku 
+https://afternoon-beach-89828.herokuapp.com/
+
+## License
+[MIT](https://choosealicense.com/licenses/mit/)
--- ripley/main.js
+++ ripley/main.js
@@ -21,49 +21,80 @@ app.use(cors());
 app.use(morgan('combined'));
 const axios = require('axios');
 var redis = require('redis');
+const WebSocket = require('ws');
+
+//const wss = new SocketServer({app})
+const wss = new WebSocket.Server({ port: 3030 });
+
 //var client = redis.createClient();
 if (process.env.REDISTOGO_URL) {
     var rtg   = require("url").parse(process.env.REDISTOGO_URL);
-    var client = require("redis").createClient(rtg.port, rtg.hostname);
+    var clientRedis = require("redis").createClient(rtg.port, rtg.hostname);
 
-    client.auth(rtg.auth.split(":")[1]);
+    clientRedis.auth(rtg.auth.split(":")[1]);
   } else {
-    var client = require("redis").createClient();
+    var clientRedis = require("redis").createClient();
   }
-client.on('connect', function() {
+  clientRedis.on('connect', function() {
     console.log('Redis client connected');
 });
 
-client.on('error', function (err) {
+clientRedis.on('error', function (err) {
     console.log('Something went wrong ' + err);
 });
-client.set('CL', '-33.4372,-70.6506', redis.print);
-client.set('CH', '47.376721, 8.546354', redis.print);
-client.set('NZ', '-36.8404,174.74', redis.print);
-client.set('AU', '-33.8667,151.2', redis.print);
-client.set('UK', '51.5072,-0.1275', redis.print);
-client.set('USA', '32.674684,-83.25066', redis.print);
+clientRedis.set('CL', '-33.4372,-70.6506', redis.print);
+clientRedis.set('CH', '47.376721, 8.546354', redis.print);
+clientRedis.set('NZ', '-36.8404,174.74', redis.print);
+clientRedis.set('AU', '-33.8667,151.2', redis.print);
+clientRedis.set('UK', '51.5072,-0.1275', redis.print);
+clientRedis.set('USA', '32.674684,-83.25066', redis.print);
 
-function sleep(time, callback) {
-    var stop = new Date().getTime();
-    while(new Date().getTime() < stop + time) {
-        ;
-    }
-    callback();
-  }
+wss.on('connection', function connection(ws) {
+    ws.on('message', function incoming(data) {  
+            
+        if (data){
+            console.log(data)
+            wss.clients.forEach(function each(client) {
+                clientRedis.get(data, function (error, result) {
+                    console.log(result)
+                    //client.send("adios")                            
+                    request('https://api.darksky.net/forecast/606403b3650cc2417e7bd95529388f7b/'+result, { json: true }, (err, resu, body) => {
+                        if (err) 
+                            { console.log("error") 
+                        } else {
+                            //console.log(resu.body.currently)
+                            //client.send("adios")
+                            client.send(JSON.stringify(resu.body.currently));
+                        }                       
+                    });  
+                    
+                });                                               
+              });
+        }
+        /*
+            setInterval(function(){
+                
+            },10*1000);
+        */
+    });
+});
+
+
   const request = require('request');
 app.get('/:id', (req, res) => {
 
    return new Promise(function(resolve,reject){
         let failvar = false;
-        client.get(req.params.id, function (error, result) {
+        clientRedis.get(req.params.id, function (error, result) {
+            //client.set('lastcallID',req.params.id,redis.print);
+            //global.varScope = req.params.id;
             do  {         
                 try {
                     failvar = false;
                     if (Math.random(0, 1) < 0.1) 
                         throw new Error('How unfortunate! The API Request Failed')
                 } catch(err) {
-                    client.hset(+ new Date(),"api.errors","Error generado en modo Random")
+                    clientRedis.hset(+ new Date(),"api.errors","Error generado en modo Random")
                     failvar = true;
                 }
                 request('https://api.darksky.net/forecast/606403b3650cc2417e7bd95529388f7b/'+result, { json: true }, (err, resu, body) => {
